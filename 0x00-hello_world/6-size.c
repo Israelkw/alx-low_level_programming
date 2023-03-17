80bdcd274fcb99d8a995a2b575e3ae28994c56fe
Israelkw <srlkbd@gmail.com>
2023-03-17 02:40:13 +0000

    x

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %ld bite(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld bite(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld bite(s)\n", sizeof(float));
-	putchar(stderr, "[Anything]");
+	fprintf(stderr, "[Anything]");
 	return (0);
 }
