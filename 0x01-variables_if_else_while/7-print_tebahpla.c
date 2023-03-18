635d87ffe9ef1ac33c6b642d4a72123f2b215b23
Israelkw <srlkbd@gmail.com>
2023-03-18 01:25:47 +0000

    if while else and variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - abc
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z' ; a <= 'a' ; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
