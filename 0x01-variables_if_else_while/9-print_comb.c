f1042fe042fef2707db4b2757cc967cfeb97abc4
Israelkw <srlkbd@gmail.com>
2023-03-18 02:41:49 +0000

    if while else and variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+/**
+ *  * main - abc
+ *   * Return: 0 Always (Success)
+ *    */
+int main(void)
+{
+	int a;
+	
+	for (a ='0' ; a <= '9' ; a++)
+	{p
+		utchar(a);
+		if(a != 9)
+		{putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
