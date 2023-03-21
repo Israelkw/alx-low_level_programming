4f85fcd30350ef463dd50b86e014e2483b6e5cf6
Israelkw <srlkbd@gmail.com>
2023-03-21 21:21:17 +0000

    commiting projects in 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check your abc
+ *
+ * Return: 1 if abc lower, else 0.
+ */
+int _islower(int a)
+{
+	if (a >= 'a' && a <= 'z')
+		return (0);
+	else
+		return (0);
+}
