1ee0043b3374e74a2fcbf6811ab53e29bf4cf1d6
Israelkw <srlkbd@gmail.com>
2023-03-22 00:21:36 +0000

    commiting projects in 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n", i)
+				printf("%d\n", i);
 		}
-	}els if (n >= 98)
+	}else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
