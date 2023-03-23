ca0686741d5c5a4bea21b9f9551190a65c5a9b87
Israelkw <srlkbd@gmail.com>
2023-03-23 21:15:48 +0000

    sleepy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
-#include <tdio.h>
+#include <stdio>
 
 /**
- * main - buzzfizz
+ * main - flysounds
  *
- * Return: always 0 (success)
+ * Return: 0
  */
 int main(void)
 {
@@ -11,28 +11,28 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
 		}
-		else if ((i % 5 == 0 && i % 3 == 0)
-				{
-				printf(" FizzBuzz");
-				}
-				else if (i == 1)
-				{
-				printf("%d", i);
-				}
-				else
-				{
-				printf("%d", i);
-				}
-				}
-				printf("\n");
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %", i);
+		}
+	}
+	printf("\n");
 
-				return (0);
-				}
+	return (0);
+}
