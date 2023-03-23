b25ba33267f546e37ee63803f8ad7fa5f44d96b0
Israelkw <srlkbd@gmail.com>
2023-03-23 15:03:30 +0000

    more functions and nestes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * mul - multiplies 2 digits
+ * @a: first digit to be in the equestion
+ * @b: second digit of the equestion
+ *
+ * Return: success (0)
+ */
+int mul(int a, int b)
+{
+	return (a * b)
+}
