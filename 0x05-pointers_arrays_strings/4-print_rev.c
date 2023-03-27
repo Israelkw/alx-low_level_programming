83e61117e93393199f60492a5d5dc8906a1fe676
Israelkw <srlkbd@gmail.com>
2023-03-27 21:31:47 +0000

    5th

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - name explanatory
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*s != '\0)
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (j = i; j > 0; j++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
