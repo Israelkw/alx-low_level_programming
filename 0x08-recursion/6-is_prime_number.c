2f5f2f51e1972a55d962a054324f68de9a86e92f
Israelkw <srlkbd@gmail.com>
2023-04-06 02:07:19 +0000

    git add git commit git push for all by alias

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int isprime(int n, int i);
+
+/**
+ * is_prime_number - prime number
+ * @n: intiger
+ * Return: prime 1 else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+	{
+		return (0);
+	}
+	return (isprime(n, n - 1));
+}
+
+/**
+ * isprime - search prime
+ * @n: int
+ * @i: int
+ * Return: prime
+ */
+int isprime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (isprime(n, i - 1));
+}
