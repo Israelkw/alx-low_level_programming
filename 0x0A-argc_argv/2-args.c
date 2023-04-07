663ea08126629289828aa0a980d8b8bb1ad62749
Israelkw <srlkbd@gmail.com>
2023-04-07 22:13:08 +0000

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all
+ * @argc: number
+ * @argv: array
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++);
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
