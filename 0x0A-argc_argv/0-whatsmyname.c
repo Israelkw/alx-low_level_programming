71ee207becb9003e84c0f03dc0e0ad6c9f3d3658
Israelkw <srlkbd@gmail.com>
2023-04-07 21:47:55 +0000

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * main - progtam name print
+ * @argc: name of arg
+ * @argv: array of the arg
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
