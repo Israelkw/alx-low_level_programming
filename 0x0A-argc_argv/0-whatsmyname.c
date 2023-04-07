6418a963e9023009aeb1c0d1266431238616c51a
Israelkw <srlkbd@gmail.com>
2023-04-07 21:55:10 +0000

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: array of the arg
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
