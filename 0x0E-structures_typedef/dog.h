9f36182179e1e8b8a5c72696e647b39c3268f14f
Israelkw <srlkbd@gmail.com>
2023-04-17 23:16:29 +0000

    alias gits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - dog info
@@ -15,6 +17,10 @@ struct dog
 	char *owner;
 };
 
-struct dog my_dog = {char *d, float age, char *owner};
+void i_dog(struct dog *, char *name, float age, char *owner);
+void p_dog(struct dog *d);
+typeof struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
