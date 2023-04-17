264b62c45f8714f46f2d9a8774721bea695d79cf
Israelkw <srlkbd@gmail.com>
2023-04-17 23:35:27 +0000

    alias gits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,11 +14,10 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 void print_dog(struct dog *d);
-typeof struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+
 #endif
