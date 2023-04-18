9eac1032d41b8914002e13ee036574a7a1d7b287
Israelkw <srlkbd@gmail.com>
2023-04-18 00:00:36 +0000

    alias gits

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - print dog
+ * @d: struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+}
