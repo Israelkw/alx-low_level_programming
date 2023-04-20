b82599170df181aadd75e9cbccc345afb3d5b441
Israelkw <srlkbd@gmail.com>
2023-04-20 01:14:13 +0000

    commited

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,10 +8,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char **argv)
 {
 	int num1, num2, result;
 	int (*op_func)(int, int);
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,12 +20,18 @@ int main(int argc, char **argv)
 		return (98);
 	}
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-	if (!op_func || argv[2][1] != '\0')
+	op_func = get_op_func(op);
+	if (!op_func || op[1] != '\0')
 	{
 		printf("Error\n");
 		return (99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	result = op_func(num1, num2);
 	printf("%d\n", result);
