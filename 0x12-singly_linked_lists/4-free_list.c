66e8934c99fede3c885d7e99fde8763f8c6addd3
Israelkw <srlkbd@gmail.com>
2023-04-27 23:17:43 +0000

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_list - frees a list
+ * @head: first
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
