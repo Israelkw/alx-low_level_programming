744661352fe7f30dfddcc2bd40e8cd6e15cfd0a4
Israelkw <srlkbd@gmail.com>
2023-05-03 19:58:57 +0000

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints
+ * @h: points
+ * Return: count
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
