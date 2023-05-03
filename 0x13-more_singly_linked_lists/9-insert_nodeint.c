6cb3faa13c5b55e6f081379050c975d77cdfa9fb
Israelkw <srlkbd@gmail.com>
2023-05-03 20:44:45 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: pointer to pointer of first node of listint_t list.
- * @idx: index of the list where the new node should be added. Index starts at 0.
+ * @idx: index of the list where the new node should be added.
  * @n: integer data for new node.
  *
  * Return: address of the new node or NULL if it fails.
