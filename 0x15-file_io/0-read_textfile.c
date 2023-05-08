b329e8bf78a638bbf55f7d0df99edb64caf9b2e4
Israelkw <srlkbd@gmail.com>
2023-05-08 22:04:08 +0000

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define BUFSIZE 1024
-
-/*
- * read_textfile - Reads text file and prints it to the POSIX standard output
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
  * @filename: The name of the file to be read
  * @letters: The number of letters it should read and print
  *
@@ -13,38 +12,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buf[BUFSIZE];
-	ssize_t fd;
+	int fd;
 	ssize_t r;
 	ssize_t w;
-	size_t total;
+	char *buf;
 
-	if (filename == NULL || letters == 0)
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		perror("open");
 		return (0);
-	}
-	total = 0;
-	while (total < letters && (r = read(fd, buf, BUFSIZE)) > 0)
-	{
-		w = write(STDOUT_FILENO, buf, r);
-		if (w == -1)
-		{
-			perror("write");
-			close(fd);
-			return (0);
-		}
-		total += w;
-	}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	r = read(fd, buf, letters);
 	if (r == -1)
-	{
-		perror("read");
-		close(fd);
 		return (0);
-	}
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1 || w != r)
+		return (0);
+	free(buf);
 	close(fd);
-	return (total);
+	return (w);
 }
